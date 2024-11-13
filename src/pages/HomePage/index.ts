ab102de864c0ef577a28e39620dab9c57a33a24a
NikitaTorbenko <2015nekitciti@gmail.com>
2024-11-13 11:54:19 +0300

    init

---
+++ src/pages/HomePage/index.ts
@@ -0,0 +1,5 @@
+export { default as HomePage } from './components/HomePage.vue'
+
+// тут точно такая же структура как и в modules/ProductList
+
+// тоже пишешь
---
+++ src/shared/ui/Button/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+interface Props {
+  title: string
+  clickFunc: () => void
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <button @click="props.clickFunc" class="btn">{{ props.title }}</button>
+</template>
+
+<style scoped lang="scss">
+.btn {
+  // тут какие-то стили для кнопочки
+}
+</style>
